3854f84505677687e91b06a4eefba7e9f9619eff
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-07-12 13:55:36 +0100

    Update streamlit_app.py
    Getting the Fruityvice Data to Show Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -47,7 +49,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
